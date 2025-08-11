75254ea803bffcb7e4d2925ef5cdcc75de721813
mohamaddoaaakila-alt <mohamad.doaa.akila@gmail.com>
2025-08-11 15:11:38 +0300

    Update CleverCryptoSignalsBot.py

--- CleverCryptoSignalsBot.py
+++ CleverCryptoSignalsBot.py
@@ -12,7 +12,7 @@ import requests
 from datetime import datetime, timedelta
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-    Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, Filters,
+    Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, filters,
     JobQueue
 )
 from cryptography.fernet import Fernet
